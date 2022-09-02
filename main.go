8cb0657f03f069124b1963f5f063a6dc39e90a1b
Kushal Ausula <88698567+kushalausula-dev@users.noreply.github.com>
2022-09-02 07:42:49 +0000

    implement function

--- main.go
+++ main.go
@@ -7,3 +7,26 @@ import (
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+func mergeTwoArrays(arr1 []int, arr2 []int) []int {
+  
+	if arr1 != nil && len(arr1) == 0 {
+		return arr2
+	}
+	if arr2 != nil && len(arr2) == 0 {
+		return arr1
+	}
+ //  array1Item:=arr1[0]
+ //  array2Item:=arr2[0]
+
+  for{
+    if
+  }
+  
+  
+
+	// process merging here if both arrays are not nil
+
+  
+
+}
